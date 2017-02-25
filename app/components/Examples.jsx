eb1b082feda7eed07991147cd3ff9f008a93f2ed
robgaston1 <robcoach0221@gmail.com>
2017-02-25 16:09:20 -0500

    Prep repo for heroku

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,14 +1,16 @@
 var React = require('react');
 
 var Examples = React.createClass({
-  render: function () {
-    return (
-      <h3>Examples Component</h3>
-    );
-  }
+    render: function() {
+        return (
+            <div>
+                <h3>Examples Component</h3>
+                <p>Welcome to the Examples page!</p>
+            </div>
+        );
+    }
 });
 
-var Examples = (props) =>
- <h3>Examples Component</h3>;
+var Examples = (props) => <h3>Examples Component</h3>;
 
 module.exports = Examples;
