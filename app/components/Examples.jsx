c77b45e246268d747f633364f93adf140255ceac
robgaston1 <robcoach0221@gmail.com>
2017-02-25 17:56:49 -0500

    Changed copy on About and fixed code on Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,16 +1,12 @@
 var React = require('react');
 
-var Examples = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <h3>Examples</h3>
-                <p>Welcome to the Examples page!</p>
-            </div>
-        );
-    }
-});
-
-var Examples = (props) => <h3>Examples Component</h3>;
+var Examples = (props) => {
+  return (
+      <div>
+          <h3>Examples</h3>
+          <p>Welcome to the Examples page!</p>
+      </div>
+  )
+};
 
 module.exports = Examples;
