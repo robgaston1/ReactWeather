db22a8207cc829746e7a849b4a7f264102ba3507
robgaston1 <robcoach0221@gmail.com>
2017-02-26 09:52:18 -0500

    Style the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,9 +11,18 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the About page</p>
+            <h1 className='text-center'>About</h1>
+            <p>This is a weather application built on React. This was built as a project in a react web developer course. </p>
+            <p>Tools used:</p>
+            <ul>
+              <li>
+                <a href='https://facebook.github.io/react'>React</a> - Link to the React Javascript framework repository.
+              </li>
+              <li>
+                <a href='http://openweathermap.org'>Open Weather Map</a> - Open Weather Map is the API that serves data for this weather application. 
+              </li>
+            </ul>
         </div>
-    )
+    );
 }
 module.exports = About;
